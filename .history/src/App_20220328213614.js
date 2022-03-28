dd8dd635f04e0d458723b236fd54f60d5684b0c8
hoang0508 <nguyenhuyhoang05082001@gmail.com>
2022-03-28 21:36:53 +0700

    hoang6

---
+++ .history/src/App_20220328213614.js
@@ -0,0 +1,42 @@
+import React, { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import Main from "./components/layout/Main";
+// import HomePage from "./components/pages/HomePage";
+// import MovieDetailsPage from "./components/pages/MovieDetailsPage";
+// import MoviePage from "./components/pages/MoviePage";
+// import TVPage from "./components/pages/TVPage";
+// import TvPageDetails from "./components/pages/TvPageDetails";
+// import WatchMoviePage from "./components/pages/WatchMoviePage";
+
+const HomePage = lazy(() => import("./components/pages/HomePage"));
+const MovieDetailsPage = lazy(() =>
+  import("./components/pages/MovieDetailsPage")
+);
+const MoviePage = lazy(() => import("./components/pages/MoviePage"));
+const TVPage = lazy(() => import("./components/pages/TVPage"));
+const TvPageDetails = lazy(() => import("./components/pages/TvPageDetails"));
+const WatchMoviePage = lazy(() => import("./components/pages/WatchMoviePage"));
+
+function App() {
+  return (
+    <>
+      <Suspense fallback={<></>}>
+        <Routes>
+          <Route element={<Main />}>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/movies" element={<MoviePage />}></Route>
+            <Route path="/movie/:id" element={<MovieDetailsPage />}></Route>
+            <Route path="/tv" element={<TVPage />}></Route>
+            <Route path="/tv/:id" element={<TvPageDetails />}></Route>
+            <Route
+              path="/movie/:id/watch/:id"
+              element={<WatchMoviePage />}
+            ></Route>
+          </Route>
+        </Routes>
+      </Suspense>
+    </>
+  );
+}
+
+export default App;
---
+++ .history/src/components/layout/Header_20220328213608.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.scss";
+// import logo from "../../assets/logo.png";
+import { FaSearch, FaBell } from "react-icons/fa";
+import { FaPlayCircle } from "react-icons/fa";
+const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <div className="container">
+          <div className="header-main">
+            <div className="header-nav">
+              <div className="header-logo">
+                <span className="header-logo--icon">
+                  <FaPlayCircle />
+                </span>
+                <span className="header-logo--text">HHmovie</span>
+              </div>
+              <div className="header-menu">
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive ? "header-active" : "header-white"
+                  }
+                >
+                  Home
+                </NavLink>
+
+                <NavLink
+                  to="/movies"
+                  className={({ isActive }) =>
+                    isActive ? "header-active" : "header-white"
+                  }
+                >
+                  Movie
+                </NavLink>
+                <NavLink
+                  to="/tv"
+                  className={({ isActive }) =>
+                    isActive ? "header-active" : "header-white"
+                  }
+                >
+                  TV Series
+                </NavLink>
+              </div>
+            </div>
+            <div className="header-noti">
+              <div className="header-search">
+                <FaSearch />
+              </div>
+              <div className="header-bell">
+                <div className="header-bell--icon">
+                  <FaBell />
+                </div>
+                <div className="header-bell--number">0</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
